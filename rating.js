5086fc129eefc7b90d4ef06d274fa49468239032
kraberry <k2jang@uwaterloo.ca>
2017-05-06 16:12:04 -0400

    Add files via upload

---
+++ rating.js
@@ -0,0 +1,17 @@
+
+// function to determine the ranking for each NGO based off of the severity of their
+//    scandals, the number of their scandals, and the percentage of their revenue that
+//    is used up by administrative costs
+function rating(scandals, numScandal, admin){
+  // declare the variables
+  var score;
+  var totalScandal;
+
+  // each one starts with a score of 100
+  score = 100;
+
+  // find the total impact of all their scandals
+  totalScandal = scandals * numScandal;
+
+  score = score - totalScandal - admin;
+}
